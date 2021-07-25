42658fc24ac54ca579eda23eba78e06085ee5373
Silas M Knight <silasfelinus@gmail.com>
2021-07-25 10:44:55 -0700

    Update app.rb
    Added get routes

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+end 
+  
+  get '/name' do
+    "My name is Silas"
+  end
+  
+  get '/hometown' do
+    "My hometown is Arcata"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is anything by Cat Stevens"
+  end
 end
